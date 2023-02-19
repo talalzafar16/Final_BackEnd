0172723733794cc8bd05cb56c2f388b9fb0bdbb3
Talal <talalzafar84@gmail.com>
2023-02-19 15:42:35 +0500

    pushing

--- App.js
+++ App.js
@@ -6,10 +6,12 @@ const mongoose= require('mongoose');
 const router  = require('./routers');
 const DBURI = "mongodb+srv://admin:admin@cluster0.6wqjbin.mongodb.net/?retryWrites=true&w=majority";
 
-mongoose.connect(DBURI).then((res) =>console.log("mongo DB connected")).catch((err) =>console.log("DBerr",err))          
-
-app.use(express.json()); 
-app.use(cors())     
-app.use(router)   
+mongoose.connect(DBURI).then((res) =>{
+    console.log("mongo DB connected")
+    app.use(express.json()); 
+    app.use(cors())     
+    app.use(router)   
+})
+.catch((err) =>console.log("DBerr",err))          
 
 app.listen(PORT, () =>console.log(`Your server is runing on http://localhost:${PORT}`));
